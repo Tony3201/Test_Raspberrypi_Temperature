04a6bc78964d1c8fc9f0820e510d60a3cc8e0b2e
Tony3201 <Tony3201@users.noreply.github.com>
2020-09-06 11:58:23 -0400

    Create Temp.c

---
+++ Temp.c
@@ -0,0 +1,17 @@
+#For test RaspberryPi3b is working  # 
+#####################################
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   FILE *fp;
+
+   int temp = 0;
+   fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+   fscanf(fp, "%d", &temp);
+   printf(">> CPU Temp: %.2f°C\n", temp / 1000.0);
+   fclose(fp);
+
+   return 0;
+}
+
